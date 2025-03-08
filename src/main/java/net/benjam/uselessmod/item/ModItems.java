5bba1dc91324cabef9171f6477caf72a2d79d2af
beeppi <bepsteri@hotmail.com>
2025-03-08 12:34:58 +0200

    formatting changes

--- src/main/java/net/benjam/uselessmod/item/ModItems.java
+++ src/main/java/net/benjam/uselessmod/item/ModItems.java
@@ -11,41 +11,58 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = 
-    DeferredRegister.create(ForgeRegistries.ITEMS, UselessMod.MOD_ID);
+    DeferredRegister.create(
+        ForgeRegistries.ITEMS,
+        UselessMod.MOD_ID
+    );
 
     public static final RegistryObject<Item> RAW_USELESSNESS =
-    ITEMS.register("raw_uselessness", () -> new Item(new Item.Properties()));
+    ITEMS.register(
+        "raw_uselessness",
+        () -> new Item(new Item.Properties())
+    );
 
     public static final RegistryObject<Item> USELESSNESS =
-    ITEMS.register("uselessness", () -> new Item(new Item.Properties()));
+    ITEMS.register(
+        "uselessness",
+        () -> new Item(new Item.Properties())
+    );
 
 
     public static final RegistryObject<Item> USELESS_HELMET =
-    ITEMS.register("useless_helmet", () -> new ArmorItem(
-        ModArmorMaterials.USELESSNESS, 
-        ArmorItem.Type.HELMET, 
-        new Item.Properties()
+    ITEMS.register(
+        "useless_helmet",
+        () -> new ArmorItem(
+            ModArmorMaterials.USELESSNESS, 
+            ArmorItem.Type.HELMET, 
+            new Item.Properties()
     ));
 
     public static final RegistryObject<Item> USELESS_CHESTPLATE =
-    ITEMS.register("useless_chestplate", () -> new ArmorItem(
-        ModArmorMaterials.USELESSNESS, 
-        ArmorItem.Type.CHESTPLATE, 
-        new Item.Properties()
+    ITEMS.register(
+        "useless_chestplate",
+        () -> new ArmorItem(
+            ModArmorMaterials.USELESSNESS, 
+            ArmorItem.Type.CHESTPLATE, 
+            new Item.Properties()
     ));
 
     public static final RegistryObject<Item> USELESS_LEGGINGS =
-    ITEMS.register("useless_leggings", () -> new ArmorItem(
-        ModArmorMaterials.USELESSNESS, 
-        ArmorItem.Type.LEGGINGS, 
-        new Item.Properties()
+    ITEMS.register(
+        "useless_leggings",
+        () -> new ArmorItem(
+            ModArmorMaterials.USELESSNESS, 
+            ArmorItem.Type.LEGGINGS, 
+            new Item.Properties()
     ));
 
     public static final RegistryObject<Item> USELESS_BOOTS =
-    ITEMS.register("useless_boots", () -> new ArmorItem(
-        ModArmorMaterials.USELESSNESS, 
-        ArmorItem.Type.BOOTS, 
-        new Item.Properties()
+    ITEMS.register(
+        "useless_boots",
+        () -> new ArmorItem(
+            ModArmorMaterials.USELESSNESS, 
+            ArmorItem.Type.BOOTS, 
+            new Item.Properties()
     ));
 
     public static final RegistryObject<Item> USELESS_SWORD = 
